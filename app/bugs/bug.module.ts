0bfabb92b33de0b7f623960a8d66d2e5902219ce
Colin Fendrick <colin.fendrick@gmail.com>
2019-10-12 07:05:48 -0400

    :sparkles: Add modal and bind

--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -5,13 +5,20 @@ import { SharedModule } from '../shared/shared.module';
 import { BugRoutingModule } from './bug-routing.module';
 // Component
 import { BugListComponent } from './bug-list/bug-list.component';
+import { BugDetailComponent } from './bug-detail/bug-detail.component';
 
 // Service
 import { BugService } from './service/bug.service';
 
 @NgModule({
-  imports: [SharedModule, BugRoutingModule],
-  declarations: [BugListComponent],
+  imports: [
+    SharedModule,
+    BugRoutingModule
+  ],
+  declarations: [
+    BugListComponent,
+    BugDetailComponent
+  ],
   exports: [],
   providers: [BugService]
 })
